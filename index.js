51901c7ae34922d8ffa0e26be9508a8b1224b4b1
delesslin <delesslingw@gmail.com>
2022-08-09 11:22:12 -0400

    :rocket: mvp of dynamic meta tags

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const contentful = require('contentful')
@@ -9,16 +10,26 @@ const client = contentful.createClient({
   accessToken: process.env.CONTENTFUL_TOKEN,
 })
 // This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
-let CONTENT = []
+let CONTENT = [],
+  HTMLdata
 
 const app = express()
-app.use(express.static(path.join(__dirname, '/build')))
 app.get('/api', (req, res) => {
   res.send(CONTENT)
 })
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/build/index.html'))
+
+const routes = ['/', '/library', '/programs', '/classes', '/thpo']
+routes.forEach((route) => {
+  app.get(route, (_, res) => res.send(HTMLdata))
 })
+
+app.get('/', (req, res) => {
+  console.log('get!')
+  // console.log(HTMLdata)
+  res.send(HTMLdata)
+})
+app.get('/')
+app.use(express.static(path.resolve(__dirname, 'build')))
 const PORT = process.env.PORT || 3333
 
 client
@@ -29,15 +40,54 @@ client
       // console.log(o.sys.contentType.sys)
       return o.sys.contentType.sys.id == 'homepage'
     })
+    return
   })
   .then(() => {
-    app.listen(PORT, (e) => {
-      if (e) {
-        console.error(e)
-        return
-      }
+    console.log('gen HTML')
+    // console.log(CONTENT[0])
+    const { homepageTitle, homepageDescription, homepageHeroImage } =
+      CONTENT[0].fields
 
-      console.log(`Listening on http://localhost:${PORT}`)
+    const indexPath = path.resolve(__dirname, 'build', 'index.html')
+    fs.readFile(indexPath, 'utf8', (err, htmlData) => {
+      if (err) {
+        console.error('Error during file reading', err)
+        return res.status(404).end()
+      }
+      console.log(homepageDescription.content[0])
+      const richTextToString = (obj) => {
+        if (obj.value) {
+          // console.log(obj.value)
+          return obj.value
+        } else {
+          return obj.content.reduce((acc, curr) => {
+            console.log(acc)
+            return `${acc}${richTextToString(curr)}`
+          }, '')
+        }
+      }
+      let description = richTextToString(homepageDescription)
+      description = description.replaceAll('"', '')
+
+      const config = {
+        __META_OG_TITLE__: homepageTitle,
+        __META_OG_IMAGE__: `https:${homepageHeroImage[0].fields.file.url}`,
+        __META_DESCRIPTION__: description,
+        __META_OG_DESCRIPTION__: description,
+      }
+      HTMLdata = Object.keys(config).reduce((acc, curr) => {
+        return acc.replace(curr, config[curr])
+      }, htmlData)
+
+      // HTMLdata = htmlData.replace('__META_OG_TITLE__', homepageTitle)
+      app.listen(PORT, (e) => {
+        if (e) {
+          console.error(e)
+          return
+        }
+
+        console.log(`Listening on http://localhost:${PORT}`)
+      })
     })
   })
   .catch(console.error)
